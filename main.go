4543278f8e84ab3c9dda1363aa64e8924e46736f
HarleyAppleChoi <appleno56@gmail.com>
2022-03-01 18:04:35 +0000

    Support floating balance

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	coingecko "github.com/superoo7/go-gecko/v3"
 )
 
-func main(){
+func main() {
 	//coingecko
 	httpClient := &http.Client{
 		Timeout: time.Second * 10,
@@ -19,63 +19,74 @@ func main(){
 	CG := coingecko.NewClient(httpClient)
 
 	// Import coin info
-	coin:=types.NewCoin("bitcoin",5.1234,
-	time.Date(2020,time.January,28,0,0,0,0,time.UTC))
-	vsCurrency:="USD"
+	// Assume the dateQuantity pair is always by time asc
+	coin := types.NewCoin("bitcoin", []types.DateQuantity{
+		types.NewDateQuantity(5.1234,
+			time.Date(2020, time.January, 28, 0, 0, 0, 0, time.UTC)),
+		types.NewDateQuantity(15.5642,
+			time.Date(2020, time.August, 28, 0, 0, 0, 0, time.UTC)),
+	})
 
-	
+	vsCurrency := "USD"
 
 	// getting balance for the address
 	// May need to query from graphql if we want to get exact balance
 	// Like what X does?
 	// For now just using the price
-	balances,err:=ParseBalanceSheet(coin,vsCurrency,CG)
-	if err!=nil{
+	balances, err := ParseBalanceSheet(coin, vsCurrency, CG)
+	if err != nil {
 		panic(err)
 	}
 
-	
 	// Output the .csv file contains the
-	// schema 
+	// schema
 	// date, coin price, account balance
-	outputcsv:=balances.GetCSV()
+	outputcsv := balances.GetCSV()
 	fmt.Println(outputcsv)
 }
 
-func ParseBalanceSheet(c types.Coin,vsCurrency string,CG *coingecko.Client)(types.Balances,error){
+//
+func ParseBalanceSheet(c types.Coin, vsCurrency string, CG *coingecko.Client) (types.Balances, error) {
 	var balances types.Balances
-	
-		// getting coinprice fact
-		days:=time.Since(c.StartDate).Hours()/24
-		// DAILTY data will be used for duration above 90 days.
-		coindata,err:=CG.CoinsIDMarketChart(c.CoinType,vsCurrency,strconv.FormatFloat(days, 'f', -1, 64))
-		if err!=nil{
-			return nil,err
-		}
-		
-		// convert it to []types.Balance
-		countmonth:=1
-		countday:=0
-		prices:=*coindata.Prices
-		for countday<len(prices){
-			
 
+	// getting coinprice fact
+	days := time.Since(c.DateQuantities[0].Date).Hours() / 24
+	// DAILTY data will be used for duration above 90 days.
+	coindata, err := CG.CoinsIDMarketChart(c.CoinType, vsCurrency, strconv.FormatFloat(days, 'f', -1, 64))
+	if err != nil {
+		return nil, err
+	}
 
-			balances = append(balances,types.NewBalance(&c,prices[countday][1],
-				c.StartDate.Add(time.Hour*time.Duration(24*countday)), 
-				prices[countday][1]*float32(c.Quantity),vsCurrency))
-			
-			countmonth+=1
-
-			if countmonth==13{
-				countmonth=1
+	// convert it to []types.Balance
+	countmonth := 1
+	countday := 0
+	initialDate:= c.DateQuantities[0].Date
+	prices := *coindata.Prices
+	for i, datequantity := range c.DateQuantities {
+		for countday < len(prices) {
+			dateNow := initialDate.Add(time.Hour * time.Duration(24*countday))
+			if i!=len(c.DateQuantities)-1 && dateNow.After(c.DateQuantities[i+1].Date) {
+				break
 			}
-			if countmonth%2==0{
-				countday+=30
-				}else{
-					countday+=31
-				}
+
+			balances = append(balances, types.NewBalance(&c, prices[countday][1],
+				dateNow,
+				prices[countday][1]*float32(datequantity.Quantity), vsCurrency))
+
+			countmonth += 1
+
+			if countmonth == 13 {
+				countmonth = 1
+			}
+			if countmonth%2 == 0 {
+				countday += 30
+			} else {
+				countday += 31
+			}
+
 		}
 
-		return balances,nil
+	}
+
+	return balances, nil
 }
--- types/types.go
+++ types/types.go
@@ -5,45 +5,57 @@ import (
 	"time"
 )
 
-type Coin struct{
-	CoinType string
+type DateQuantity struct {
 	Quantity float64
-	StartDate time.Time
+	Date     time.Time
 }
 
-func NewCoin(cointype string, quantity float64, startDate time.Time)Coin{
-	return Coin{
-		CoinType: cointype,
+func NewDateQuantity(quantity float64, date time.Time) DateQuantity {
+	return DateQuantity{
 		Quantity: quantity,
-		StartDate: startDate,
+		Date:     date,
 	}
 }
 
-type Balance struct{
-	Coin *Coin
-	VSCurrency string
-	Price float32 // The coin price on that time
-	Date time.Time // The time that the balance due
-	Balance float32 // The balance on that account
+// An coin history of how the balance in a account flow
+type Coin struct {
+	CoinType       string
+	DateQuantities []DateQuantity
 }
 
-func NewBalance(coin *Coin, price float32, date time.Time,balance float32,vSCurrency string)Balance{
+func NewCoin(cointype string, dateQuantities []DateQuantity) Coin {
+	return Coin{
+		CoinType:       cointype,
+		DateQuantities: dateQuantities,
+	}
+}
+
+// This represent a row in .csv file
+type Balance struct {
+	Coin       *Coin
+	VSCurrency string
+	Price      float32   // The coin price on that time
+	Date       time.Time // The time that the balance due
+	Balance    float32   // The balance on that account
+}
+
+func NewBalance(coin *Coin, price float32, date time.Time, balance float32, vSCurrency string) Balance {
 	return Balance{
-		Coin:coin,
-		Price: price,
-		Date:date,
-		Balance:balance,
+		Coin:       coin,
+		Price:      price,
+		Date:       date,
+		Balance:    balance,
 		VSCurrency: vSCurrency,
 	}
 }
 
 type Balances []Balance
 
-func (v Balances) GetCSV() string{
-	outputcsv:="date,coin,VSCurrency coin price, account balance\n"
-	for _,b:=range v{
-		outputcsv+=fmt.Sprintf("%s,%s,%s,%f,%f\n",
-			b.Date.String(),b.Coin.CoinType,b.VSCurrency,b.Price,b.Balance)
+func (v Balances) GetCSV() string {
+	outputcsv := "date,coin,VSCurrency coin price, account balance\n"
+	for _, b := range v {
+		outputcsv += fmt.Sprintf("%s,%s,%s,%f,%f\n",
+			b.Date.String(), b.Coin.CoinType, b.VSCurrency, b.Price, b.Balance)
 	}
 	return outputcsv
 }
